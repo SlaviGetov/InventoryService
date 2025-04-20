5babe0ea53546db6bbe48be84cf412e7884d8d5b
Pepihristova <45016031+Pepihristova@users.noreply.github.com>
2025-04-20 22:28:05 +0300

    Delete products/first part - products directory

--- products/first part - products/rest/IllegalArgumentError.js
+++
@@ -1,8 +0,0 @@
-class IllegalArgument extends Error {
-    constructor(errorMessage) {
-        super(errorMessage);
-        this.id = "illegal_argument_error";
-    }
-}
-
-export default IllegalArgument;
